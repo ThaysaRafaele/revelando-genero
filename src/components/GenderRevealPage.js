118d732f92fd75271885789700dfcfa89819b588
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2023-07-14 12:33:40 -0400

    feat: ajuste animação

--- src/components/GenderRevealPage.js
+++ src/components/GenderRevealPage.js
@@ -1,34 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import GenderReveal from './GenderReveal';
+import { motion } from 'framer-motion';
 
 const GenderRevealPage = () => {
-  const [revealed, setRevealed] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+    const [isBoxOpen, setIsBoxOpen] = useState(false);
+    const [isAnimationComplete, setIsAnimationComplete] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setIsAnimationComplete(true);
+        }, 1000);
+    
+        return () => clearTimeout(timer);
+      }, []);
 
-    return () => clearTimeout(timer);
-  }, []);
+    const handleBoxClick = () => {
+        setIsBoxOpen(true);
+    };
 
-  const revealGender = () => {
-    setRevealed(true);
-  };
-
-  return (
-    <div>
-      <h1>Revelação de Gênero</h1>      
-        <>
-          {revealed ? (
-            <h2 className="gender-reveal-text">É uma menina!</h2>
-          ) : (
-            <GenderReveal />
-          )}
-        </>
-    </div>
-  );
+    return (
+        <div className="gender-reveal-page">
+            <h1>Prepare seu coração (e colinho)</h1>
+            <h1>Vamos revelar o gênero:</h1>
+            {isBoxOpen ? (
+                <>
+                    <h2 className="gender-reveal-text">É uma menina!</h2>
+                    <img src={process.env.PUBLIC_URL + '/imagens/flork-menina.png'} alt="Imagem de Menina" className="gender-reveal-image" />
+                </>
+            ) : (
+                <motion.img
+                    src={process.env.PUBLIC_URL + '/imagens/presente-removebg-preview.png'}
+                    alt="Imagem de Presente"
+                    className={`box ${isAnimationComplete ? 'box-open' : ''}`}
+                    onClick={handleBoxClick}
+                    initial={{ x: '-100%' }}
+                    animate={{ x: 0 }}
+                    transition={{ type: 'spring', duration: 1 }}
+                />
+            )}
+        </div>
+    );
 };
 
 export default GenderRevealPage;
